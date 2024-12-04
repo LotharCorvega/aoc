fb55303577a7a9e0df50dc6ec7d366b6da4085bc
Lothar <idk>
2024-12-04 11:36:06 +0100

    day03

---
+++ 2024/day03/fst.py
@@ -0,0 +1,10 @@
+import re
+
+lines = open("input.txt", "r").read().split("\n")
+s = 0
+
+for line in lines:
+    m = re.findall(r"mul\((\d+),(\d+)\)", line)
+    s += sum(map(lambda x: int(x[0]) * int(x[1]), m))
+
+print(s)
---
+++ 2024/day03/snd.py
@@ -0,0 +1,18 @@
+import re
+
+lines = open("input.txt", "r").read().split("\n")
+active = True
+s = 0
+
+for line in lines:
+    matches = re.findall(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))", line)
+
+    for a, b, do, dont in matches:
+        if do:
+            active = True
+        elif dont:
+            active = False
+        elif active:
+            s += int(a) * int(b)
+
+print(s)
