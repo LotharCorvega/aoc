602c7463b433872ba4197fe63392fec998d1ec37
Lothar <idk>
2024-12-04 11:33:26 +0100

    day01

---
+++ 2024/day01/fst.py
@@ -0,0 +1,17 @@
+lines = open("input.txt", "r").read().split("\n")
+fst, snd = [], []
+
+for line in lines:
+    a, b = line.split()
+    fst.append(int(a))
+    snd.append(int(b))
+
+fst = sorted(fst)
+snd = sorted(snd)
+
+diff = 0
+
+for i in range(len(fst)):
+    diff += abs(fst[i] - snd[i])
+
+print(diff)
---
+++ 2024/day01/snd.py
@@ -0,0 +1,14 @@
+lines = open("input.txt", "r").read().split("\n")
+fst, snd = [], []
+
+for line in lines:
+    a, b = line.split()
+    fst.append(int(a))
+    snd.append(int(b))
+
+similarity = 0
+
+for x in fst:
+    similarity += snd.count(x) * x
+
+print(similarity)
