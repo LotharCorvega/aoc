86c5d1e3722a2b747b5bb5de5d4e1ab9a88aa32b
lot <lothar@lothar.org>
2024-12-07 12:18:32 +0100

    day07

---
+++ 2024/day07/fst.py
@@ -0,0 +1,27 @@
+from itertools import product
+
+lines = open("input.txt", "r").read().split("\n")
+sum = 0
+
+for line in lines:
+    splt = line.split(":")
+    lhs, rhs = int(splt[0]), list(map(int, splt[1].split()))
+    n = len(rhs)
+
+    for bits in product((True, False), repeat=(n - 1)):
+        x = rhs[0]
+
+        for i in range(1, n):
+            if bits[i - 1]:
+                x += rhs[i]
+            else:
+                x *= rhs[i]
+
+            if x > lhs:
+                break
+
+        if x == lhs:
+            sum += lhs
+            break
+
+print(sum)
---
+++ 2024/day07/snd.py
@@ -0,0 +1,29 @@
+from itertools import product
+
+lines = open("input.txt", "r").read().split("\n")
+sum = 0
+
+for line in lines:
+    splt = line.split(":")
+    lhs, rhs = int(splt[0]), list(map(int, splt[1].split()))
+    n = len(rhs)
+
+    for bits in product((0, 1, 2), repeat=(n - 1)):
+        x = rhs[0]
+
+        for i in range(1, n):
+            if bits[i - 1] == 0:
+                x += rhs[i]
+            elif bits[i - 1] == 1:
+                x *= rhs[i]
+            else:
+                x = int(f"{x}{rhs[i]}")
+
+            if x > lhs:
+                break
+
+        if x == lhs:
+            sum += lhs
+            break
+
+print(sum)
