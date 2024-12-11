b191067b789b337fcdcb1a98b515d968a98860a8
lot <idk>
2024-12-11 08:26:48 +0100

    day11

---
+++ 2024/day11/fst.py
@@ -0,0 +1,21 @@
+from functools import cache
+
+stones = list(map(int, open("input.txt", "r").read().strip().split()))
+
+@cache
+def sim(n, steps):
+    if steps == 0:
+        return 1
+
+    ns = str(n)
+
+    if n == 0:
+        return sim(1, steps - 1)
+    elif len(ns) % 2 == 0:
+        fst = int(ns[:len(ns)//2])
+        snd = int(ns[len(ns)//2:])
+        return sim(fst, steps - 1) + sim(snd, steps - 1)
+    else:
+        return sim(n * 2024, steps - 1)
+
+print(sum(map(lambda x: sim(x, 25), stones)))
